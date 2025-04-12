853974c5f8a468f055f1682f6e00976e34ff4b49
indkhan <mgsukhan@gmail.com>
2025-04-12 03:07:47 +0200

    basic llm and gitignore

---
+++ llmm.py
@@ -0,0 +1,33 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+from langchain_core.prompts import ChatPromptTemplate
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash-001",
+    temperature=0,
+)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant that translates {input_language} to {output_language}.",
+        ),
+        ("human", "{input}"),
+    ]
+)
+
+chain = prompt | llm
+result = chain.invoke(
+    {
+        "input_language": "English",
+        "output_language": "German",
+        "input": "I love programming.",
+    }
+)
+
+print(result)
